b89c95cd9f2eda7b6776325a8415a8204269b6f0
user <user@MacBook-Pro-user.local>
2025-03-27 12:10:14 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,25 @@
+'use strict';
+
 /**
  * @param {number} days
  *
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const dailyCarRentalCost = 40;
+  const discountForSevenDays = 50;
+  const discountForThreeDays = 20;
+  const totalAmount = dailyCarRentalCost * days;
+
+  if (days >= 7) {
+    return totalAmount - discountForSevenDays;
+  }
+
+  if (days >= 3) {
+    return totalAmount - discountForThreeDays;
+  }
+
+  return totalAmount;
 }
 
 module.exports = calculateRentalCost;
